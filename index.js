78cc45c30c73bced7a27fa28a46816b86ba7178c
PauljohnCancino <johnpaulfisher761@gmail.com>
2024-05-25 18:14:12 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const btnLike1 = document.getElementById("btnLike1")
+const countLike1 = document.getElementById("countLike1") 
+
+function clickLike1(){
+ let totalLike = parseInt(countLike1.value) + 1
+  countLike1.textContent = totalLikes.toString() 
+}
